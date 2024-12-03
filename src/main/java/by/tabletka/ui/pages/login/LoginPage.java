e0a5ecdd9e5c949a8dc2ebceaae89506458a47b1
Yuliya <yuliya.01707@gmail.com>
2024-12-03 21:51:02 +0100

    add LoginPage class

---
+++ src/main/java/by/tabletka/ui/pages/login/LoginPage.java
@@ -0,0 +1,43 @@
+package by.tabletka.ui.pages.login;
+
+import by.tabletka.ui.driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage() {
+        WebDriver driver = Driver.getDriver();
+        this.driver = driver;
+    }
+
+    public LoginPage openLoginForm() {
+        By buttonOpenLoginFormBy = By.xpath(LoginPageXpath.BUTTON_LOGIN_ON_MAIN_PAGE_XPATH);
+        WebElement buttonOpenLoginFormWebElement = this.driver.findElement(buttonOpenLoginFormBy);
+        buttonOpenLoginFormWebElement.click();
+        return this;
+    }
+
+    public LoginPage fillEmail(String email) {
+        By inputEmailBy = By.xpath(LoginPageXpath.INPUT_EMAIL_XPATH);
+        WebElement inputEmailWebElement = this.driver.findElement(inputEmailBy);
+        inputEmailWebElement.sendKeys(email);
+        return this;
+    }
+
+    public LoginPage fillPassword(String password) {
+        By inputPasswordBy = By.xpath(LoginPageXpath.INPUT_PASSWORD_XPATH);
+        WebElement inputPasswordWebElement = this.driver.findElement(inputPasswordBy);
+        inputPasswordWebElement.sendKeys(password);
+        return this;
+    }
+
+    public LoginPage clickLoginButton() {
+        By buttonLoginBy = By.xpath(LoginPageXpath.BUTTON_LOGIN_ON_FORM_XPATH);
+        WebElement buttonLoginWebElement = this.driver.findElement(buttonLoginBy);
+        buttonLoginWebElement.click();
+        return this;
+    }
+}
