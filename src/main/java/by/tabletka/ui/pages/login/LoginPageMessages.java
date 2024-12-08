3ddade37c84c472d68ed917bd1e8dab89925788f
Yuliya <yuliya.01707@gmail.com>
2024-12-08 20:43:14 +0100

    refactor LoginPageMessages

--- src/main/java/by/tabletka/ui/pages/login/LoginPageMessages.java
+++ src/main/java/by/tabletka/ui/pages/login/LoginPageMessages.java
@@ -3,6 +3,6 @@ package by.tabletka.ui.pages.login;
 public class LoginPageMessages {
     public static final String FIELD_IS_REQUIRED_MESSAGE = "Поле обязательное для заполнения";
     public static final String INVALID_EMAIL_MESSAGE = "You have not given a correct e-mail address";
-    public static final String MIN_PASSWORD_IS_3_SYMBOLS_MESSAGE = "Введите минимум 3 символа";
+    public static final String INVALID_PASSWORD_LENGTH_MESSAGE = "Введите минимум 3 символа";
     public static final String INVALID_LOGIN_DATA_MESSAGE = "Не найден пользователь или неправильный пароль.";
 }
