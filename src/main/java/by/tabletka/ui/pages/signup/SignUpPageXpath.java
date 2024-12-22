1fa89a9846e53f09a4635f182ef6abefbe317b28
Yuliya <yuliya.01707@gmail.com>
2024-12-22 21:36:51 +0100

    add SignUpPageXpath

---
+++ src/main/java/by/tabletka/ui/pages/signup/SignUpPageXpath.java
@@ -0,0 +1,11 @@
+package by.tabletka.ui.pages.signup;
+
+public class SignUpPageXpath {
+    public static final String SIGNUP_BUTTON_XPATH_ON_LOGIN_MODAL = "//*[@data-modal='login']//*[@href='/signup']";
+    public static final String EMAIL_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-email']";
+    public static final String NAME_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-name']";
+    public static final String PASSWORD_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-password']";
+    public static final String CONFIRM_PASSWORD_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-confirm']";
+    public static final String CONFIRM_CHECKBOX_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-agree']";
+    public static final String SIGNUP_BUTTON_ON_SIGNUP_FORM = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='bttn-reg']";
+}
