820510e490cd79ae65ee2629de2e5e67f9ac9e14
Yuliya <yuliya.01707@gmail.com>
2024-12-23 20:14:07 +0100

    refactor SignUpPageMessages

--- src/main/java/by/tabletka/ui/pages/signup/SignUpPageMessages.java
+++ src/main/java/by/tabletka/ui/pages/signup/SignUpPageMessages.java
@@ -1,9 +1,10 @@
 package by.tabletka.ui.pages.signup;
 
 public class SignUpPageMessages {
-    public static final String FIELD_IS_REQUIRED_MESSAGE = "Поле обязательно для заполнения!";
+    public static final String EMAIL_FIELD_IS_REQUIRED_MESSAGE = "Поле обязательно для заполнения!";
+    public static final String FIELD_IS_REQUIRED_MESSAGE = "Поле обязательное для заполнения";
     public static final String INVALID_EMAIL_MESSAGE = "Введите корректный адрес!";
     public static final String INVALID_LENGTH_MESSAGE = "Введите минимум 3 символа";
-    public static final String NOT_MATCHED_PASSWORDS = "Пароли не совпадают";
+    public static final String PASSWORDS_ARE_NOT_MATCHED_MESSAGE = "Пароли не совпадают";
     public static final String REGISTERED_USER_MESSAGE = "ошибка - пользователь с таким email уже зарегистрирован";
 }
