20a2216ceecfd15f1e5bf433daeadfbc77e0bf3a
Yuliya <yuliya.01707@gmail.com>
2024-12-23 23:09:34 +0100

    add LoginApiRequest

--- src/main/java/by/tabletka/api/login/LoginApiRequest.java
+++ src/main/java/by/tabletka/api/login/LoginApiRequest.java
@@ -1,43 +1,34 @@
 package by.tabletka.api.login;
 
-import io.restassured.builder.RequestSpecBuilder;
+import by.tabletka.api.BaseApiRequest;
 import io.restassured.specification.RequestSpecification;
 
-public class LoginApiRequest {
-    public static RequestSpecification requestSpecification;
-
-    public static void initRequestSpecificationToLogin() {
-        requestSpecification = new RequestSpecBuilder()
-                .setBaseUri("https://tabletka.by/")
-                .setBasePath("ajax-request/login")
-                .setContentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .addCookie("PHPSESSID=sn3lduti1bcgr0j067kdbnvjem; _csrf=5f79393169d6d1dd663e5835b400145970558c8b1071cc61e563ae1cb44379b2a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22SUKKXsEYZjIIRRpuZOj7r4JjZhayzYBm%22%3B%7D; _ga=GA1.1.1948258482.1730994936; allow-cookies=; _ym_uid=1732206771965883905; _ym_d=1732206771; _ym_isad=2; _ym_visorc=b; regionId=0; _ga_S6LL4MRH46=GS1.1.1733686216.14.1.1733686631.0.0.0")
-                .addHeader("X-Requested-With", "XMLHttpRequest")
-                .build();
+public class LoginApiRequest extends BaseApiRequest {
+    public LoginApiRequest(String baseUrl) {
+        super(baseUrl);
     }
 
-    public static String getBodyLoginRequest(String email, String password) {
-        return "_csrf=kT96ufzuKLLm3zGw7HXmqA3KHAXnhrVvBiPYRNJ68UrCajHypJ1t67y1ePm-J5bdV4V2MpWy_wVcS7k9qCOzJw%3D%3D&email="
+    public RequestSpecification initRequestSpecificationToLogin() {
+        return this.initRequestSpecification().basePath("ajax-request/login");
+    }
+
+    public String getBodyLoginRequest(String email, String password) {
+        return "_csrf="+this.csrfTokenBody+"&email="
                 + email + "&password=" + password + "&rememberMe=on";
     }
 
-    public static void initRequestSpecificationToSignup() {
-        requestSpecification = new RequestSpecBuilder()
-                .setBaseUri("https://tabletka.by/")
-                .setBasePath("ajax-request/signup")
-                .setContentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .addCookie("PHPSESSID=sn3lduti1bcgr0j067kdbnvjem; _csrf=5f79393169d6d1dd663e5835b400145970558c8b1071cc61e563ae1cb44379b2a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22SUKKXsEYZjIIRRpuZOj7r4JjZhayzYBm%22%3B%7D; _ga=GA1.1.1948258482.1730994936; allow-cookies=; _ym_uid=1732206771965883905; _ym_d=1732206771; _ym_isad=2; _ym_visorc=b; regionId=0; _ga_S6LL4MRH46=GS1.1.1733945220.16.1.1733945353.0.0.0")
-                .addHeader("X-Requested-With", "XMLHttpRequest")
-                .build();
+   /* public RequestSpecification initRequestSpecificationToSignup() {
+        return this.initRequestSpecification().basePath("ajax-request/signup");
     }
 
-    public static String getBodySignupRequest(String regEmail, String regName, String regPassword, String regConfirmPassword, boolean regAgree) {
+    public String getBodySignupRequest(String regEmail, String regName, String regPassword, String regConfirmPassword, boolean regAgree) {
         String regAgreeString = regAgree ? "on" : "off";
 
-        return "_csrf=YF94SYo41hL3bGCPn8aVa7LBGuZA9ITRvBj7xmdvTsczCjMC0kuTS60GKcbNlOUe6I5w0TLAzrvmcJq_HTYMqg%3D%3D&reg-email=" + regEmail
+        return "_csrf=" + this.csrfTokenBody
+                + "&reg-email=" + regEmail
                 + "&reg-name=" + regName
                 + "&reg-password=" + regPassword
                 + "&reg-confirm=" + regPassword
                 + "&reg-agree=" + regAgreeString;
-    }
+    }*/
 }
