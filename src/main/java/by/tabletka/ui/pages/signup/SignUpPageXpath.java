e736b8a96615ac5fe4048361fe046729b5afbddc
Yuliya <yuliya.01707@gmail.com>
2024-12-23 20:13:55 +0100

    refactor SignUpPageXpath

--- src/main/java/by/tabletka/ui/pages/signup/SignUpPageXpath.java
+++ src/main/java/by/tabletka/ui/pages/signup/SignUpPageXpath.java
@@ -6,7 +6,7 @@ public class SignUpPageXpath {
     public static final String NAME_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-name']";
     public static final String PASSWORD_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-password']";
     public static final String CONFIRM_PASSWORD_INPUT_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-confirm']";
-    public static final String CONFIRM_CHECKBOX_XPATH = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='reg-agree']";
+    public static final String CONFIRM_CHECKBOX_XPATH = "//label[@for='reg-agree']";
     public static final String SIGNUP_BUTTON_ON_SIGNUP_FORM = "//*[@class='js-validate reg-form has-validation-callback']//*[@id='bttn-reg']";
     public static final String ERROR_MESSAGE_EMAIL_XPATH = "//*[@id='reg-email-wrap']//*[@class='help-block form-error']";
     public static final String ERROR_MESSAGE_NAME_XPATH = "//*[@id='reg-name-wrap']//*[@class='help-block form-error']";
