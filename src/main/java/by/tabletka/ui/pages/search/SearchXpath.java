c3b90b7bb0b034ff3496cbc28cda0bdffb8cbac0
Yuliya <yuliya.01707@gmail.com>
2024-12-24 20:52:20 +0100

    add SearchXpath

---
+++ src/main/java/by/tabletka/ui/pages/search/SearchXpath.java
@@ -0,0 +1,9 @@
+package by.tabletka.ui.pages.search;
+
+public class SearchXpath {
+    public static final String INPUT_REGION_XPATH = "//*[@class='header-wrap js-header original']//*[@class='header-search-region']//*[@type='search']";
+    public static final String SELECT_REGION_KOPYL_XPATH = "//*[@class='header-wrap js-header original']//*[@class='select-check-list region-select-list']//*[@value='163']";
+    public static final String INPUT_SEARCH_REQUEST_XPATH = "//*[@class='header-wrap js-header original']//*[@class='header-search-medicine']//*[@type='search']";
+    public static final String NAME_OF_FIRST_ITEM_IN_SEARCH_RESULT = "//table[@id='base-select']//*[@class='name tooltip-info']//*[@class='tooltip-info-header']//a";
+    public static final String SEARCH_BUTTON_XPATH = "//*[@class='header-wrap js-header original']//button[@class='header-search-bttn' and @type='submit']";
+}
