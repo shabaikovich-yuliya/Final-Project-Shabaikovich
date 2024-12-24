3000f1fc815e49cc1514f2d189b0b1b414c8151d
Yuliya <yuliya.01707@gmail.com>
2024-12-25 00:22:05 +0100

    refactor GenerationDataUtil

--- src/main/java/by/tabletka/utils/GenerationDataUtil.java
+++ src/main/java/by/tabletka/utils/GenerationDataUtil.java
@@ -5,6 +5,9 @@ import com.github.javafaker.Faker;
 public class GenerationDataUtil {
 
     public static String EMPTY_VALUE = "";
+    public static String SEARCH_REQUEST = "аспирин кардио";
+    public static String SEARCH_REQUEST_FOR_NO_RESULTS = "этот запрос получает отсутсвие результата поиска";
+
 
     public static String generateEmail() {
         Faker fakerEmail = new Faker();
