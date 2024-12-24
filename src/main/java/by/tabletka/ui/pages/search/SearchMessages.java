ec926493d8deca1fbec5d9da6eb111a3113d023a
Yuliya <yuliya.01707@gmail.com>
2024-12-24 21:12:23 +0100

    add SearchMessages

---
+++ src/main/java/by/tabletka/ui/pages/search/SearchMessages.java
@@ -0,0 +1,5 @@
+package by.tabletka.ui.pages.search;
+
+public class SearchMessages {
+    public static final String NO_RESULTS_BY_THIS_REQUEST = "По вашему запросу ничего не найдено. Что можно сделать:";
+}
