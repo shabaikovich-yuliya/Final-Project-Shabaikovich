7736809558b31bea89ca5d68a758e8624b21bc11
Yuliya <yuliya.01707@gmail.com>
2024-12-25 00:21:17 +0100

    add Search and SearchXpath

--- src/main/java/by/tabletka/ui/pages/search/Search.java
+++ src/main/java/by/tabletka/ui/pages/search/Search.java
@@ -3,6 +3,9 @@ package by.tabletka.ui.pages.search;
 import by.tabletka.ui.driver.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Search {
     private WebDriver driver;
@@ -30,5 +33,21 @@ public class Search {
     public Search clickSearchButton() {
         driver.findElement(By.xpath(SearchXpath.SEARCH_BUTTON_XPATH)).click();
         return this;
+    }
+
+    public WebElement getSearchResultTable() {
+        return driver.findElement(By.xpath(SearchXpath.SEARCH_RESULT_TABLE));
+    }
+
+    public String getSearchPageTitle() {
+      return driver.findElement(By.xpath(SearchXpath.SEARCH_RESULT_TITLE_XPATH)).getText();
+    }
+
+    public WebElement getSearchPageTitleNoResults() {
+        return driver.findElement(By.xpath(SearchXpath.HEADER_NO_RESULTS_BY_THIS_REQUEST_XPATH));
+    }
+
+    public List<WebElement> getSearchResults() {
+        return driver.findElements(By.xpath(SearchXpath.NAME_OF_ITEMS_IN_SEARCH_RESULT));
     }
 }
--- src/main/java/by/tabletka/ui/pages/search/SearchXpath.java
+++ src/main/java/by/tabletka/ui/pages/search/SearchXpath.java
@@ -4,8 +4,9 @@ public class SearchXpath {
     public static final String LIST_OF_REGIONS_XPATH = "//*[@class='header-wrap js-header original']//*[@class='header-search-region']//*[@type='search']";
     public static final String SELECT_REGION_KOPYL_XPATH = "//*[@class='header-wrap js-header original']//*[@class='select-check-list region-select-list']//*[@value='163']";
     public static final String INPUT_SEARCH_REQUEST_XPATH = "//*[@class='header-wrap js-header original']//*[@class='header-search-medicine']//*[@type='search']";
-    public static final String NAME_OF_FIRST_ITEM_IN_SEARCH_RESULT = "//table[@id='base-select']//*[@class='name tooltip-info']//*[@class='tooltip-info-header']//a";
+    public static final String NAME_OF_ITEMS_IN_SEARCH_RESULT = "//table[@id='base-select']//*[@class='name tooltip-info']//*[@class='tooltip-info-header']//a";
     public static final String SEARCH_BUTTON_XPATH = "//*[@class='header-wrap js-header original']//button[@class='header-search-bttn' and @type='submit']";
-    public static final String PAGE_TITLE_IS_SEARCH_REQUEST_XPATH = "h1[class='page-title']";
+    public static final String SEARCH_RESULT_TABLE = "//table[@id='base-select']";
+    public static final String SEARCH_RESULT_TITLE_XPATH = "//h1[@class='page-title']";
     public static final String HEADER_NO_RESULTS_BY_THIS_REQUEST_XPATH = "//h2[@class='title-h2 page-title']";
 }
